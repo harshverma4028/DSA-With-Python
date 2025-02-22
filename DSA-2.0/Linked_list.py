5f7159ba44e2626b3476320975aa9fe64b303178
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-02-22 11:57:04 +0530

    hw insertion by after value and deletion by value

--- DSA-2.0/Linked_list.py
+++ DSA-2.0/Linked_list.py
@@ -87,13 +87,74 @@ class Linke_list:
             itr = itr.next
             count +=1
 
+    def insert_after_value(self,value,data):
+        if self.head is None:
+            print("Empty linked list: ")
+            return
+
+        itr = self.head
+
+        while itr:
+            if itr.data == value:
+                itr.next = Node(data,itr.next)
+                return
+            itr = itr.next
+        print("value not found ")
+    
+
+    def remove_by_value(self,value):
+        if self.head is None:
+            # print("No vlaue in the list :")
+            return
+        itr = self.head
+        if itr.data == value:
+            itr = self.head.next
+            print(f" head deleted {value}")
+            return
+
+        if self.head.data == None:
+            self.head = self.head.next
+            print("Only one value was present in the list")
+            return
+        
+
+        prev = self.head
+        cur = self.head.next
+        while cur:
+            if cur.data == value :
+                prev.next = cur.next
+                print(f"Removed node with value {value}")
+                return
+            prev = cur
+            cur = cur.next
+        print(f"value not found in the list {value} ")    
+
+
+
+
 if __name__ =='__main__':
     ll = Linke_list() 
-    ll.insert_values(["apple","cherry","grapes","coconut"])
+    ll.insert_values(["banana","mango","grapes","orange"])
     ll.print()
-    print("the length of ll is :", ll.get_length())
-    # ll.remove_at(1)
-    ll.insert_at(0,"fizz")
+    ll.insert_after_value("mango","apple") # insert apple after mango
     ll.print()
-    ll.insert_at(2,"banana")
+    ll.remove_by_value("orange") # remove orange from linked list
     ll.print()
+    ll.remove_by_value("figs")
+    ll.print()
+    ll.remove_by_value("banana")
+    ll.remove_by_value("mango")
+    ll.remove_by_value("apple")
+    ll.remove_by_value("grapes")
+    ll.print()
+    # ll.insert_values(["apple","cherry","grapes","coconut"])
+    # ll.print()
+    # print("the length of ll is :", ll.get_length())
+    # # ll.remove_at(1)
+    # ll.insert_at(0,"fizz")
+    # ll.print()
+    # ll.insert_at(2,"banana")
+    # ll.print()
+    # ll.remove_at(2)
+    # ll.print()
+
