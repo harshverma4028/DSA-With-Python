70c8992dfc176b4d207275136ca75c4337eac93f
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-03-20 18:57:49 +0530

    in order traversal binary tree

---
+++ DSA-2.0/BinaryTree/BinarySearchTree.py
@@ -0,0 +1,57 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self,data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            # Adding in the lef side
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)            
+    
+
+    def in_order_traversal(self):
+        elements = []
+
+        # visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        # visit base node 
+        elements.append(self.data)
+
+        # visit right
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements        
+    
+    def search(self, val):
+        if self.data == val:
+            
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+
+
+if __name__ == '__main__':
+    numbers = [17,4,1,20,9,23,18,34]
+    number_tree = build_tree(numbers)
+    print(number_tree.in_order_traversal())
