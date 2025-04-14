d10d75dd4476ca7a34871164c07902314cc9b2cd
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-04-14 10:37:00 +0530

    sorting using key value

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -39,7 +39,49 @@ def merge_two_sorted_lists(a,b,arr):
         k +=1
 
 
+def merge_sort1(elements , key ,descending=False):
+    size = len(elements)
+
+    if size == 1:
+        return elements
+    
+    left_list = merge_sort1(elements[0:size//2], key, descending)
+    right_list= merge_sort1(elements[size//2:],key , elements)
+    sorted_list = merge(left_list,right_list,key,descending)
+
+    return sorted_list
+
+def merge(left_list,right_list, key, descending=False):
+    merged = []
+    if descending:
+        while len(left_list) > 0 and len(right_list) > 0:
+            if left_list[0][key] >= right_list[0][key]:
+                merged.append(left_list.pop(0))
+            else:
+                merged.append(right_list.pop(0))
+
+    else:           
+        while len(left_list) > 0 and len(right_list) > 0:
+            if left_list[0][key] <= right_list[0][key]:
+                merged.append(left_list.pop(0))
+            else:
+                merged.append(right_list.pop(0))
+
+    merged.extend(left_list)
+    merged.extend(right_list)
+    return merged
+
 if __name__ == '__main__':
-    arr = [5,3,6,8,2,1,8,20,12,52,63,74,12,31,45]
-    merge_sort(arr)
-    print(arr)
+    # arr = [5,3,6,8,2,1,8,20,12,52,63,74,12,31,45]
+    # merge_sort(arr)
+    # print(arr)
+
+    elements = [
+        { 'name': 'vedanth',   'age': 17, 'time_hours': 1},
+        { 'name': 'rajab', 'age': 12,  'time_hours': 3},
+        { 'name': 'vignesh',  'age': 21,  'time_hours': 2.5},
+        { 'name': 'chinmay',  'age': 24,  'time_hours': 1.5},
+    ]
+
+    sorted_list = merge_sort1(elements ,key='time_hours', descending=True)
+    print(sorted_list)
